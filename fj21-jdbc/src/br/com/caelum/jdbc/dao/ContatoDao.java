cf5f85c779b7bffac390f7d0a436043e211442c0
Gustavo Grauzely <ggrauzely.git@gmail.com>
2017-11-16 15:50:36 -0200

    Commint do projeto caelum JDBC - criados novos metodos e classes

--- fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDao.java
+++ fj21-jdbc/src/br/com/caelum/jdbc/dao/ContatoDao.java
@@ -22,10 +22,11 @@ public class ContatoDao {
 
 	public void adiciona(Contato contato) {
 		String sql = "insert into contatos "
-				+ "(id,nome,email,endereco,dataNascimento)" + " values (?,?,?,?,?)";
+				+ "(id,nome,email,endereco,dataNascimento)"
+				+ " values (?,?,?,?,?)";
 
 		try {
-			// prepared statement para inserção
+			// prepared statement para inserÃ§Ã£o
 			PreparedStatement stmt = conn.prepareStatement(sql);
 
 			// seta os valores
@@ -40,35 +41,64 @@ public class ContatoDao {
 			stmt.execute();
 			stmt.close();
 		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+
+	public List<Contato> getLista() {
+		try {
+			List<Contato> contatos = new ArrayList<Contato>();
+
+			PreparedStatement stmt = this.conn
+					.prepareStatement("Select * from contatos");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				// criando o objeto Contato
+				Contato contato = new Contato();
+				contato.setId(rs.getLong("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	public void altera(Contato contato) {
+		String sql = "update contatos set nome=?, email=?, endereco=?,"
+				+ "dataNascimento=? where id=?";
+		try {
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento()
+					.getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
-
 	}
-	
-	public List<Contato> getLista(){
-	try{	
-		List<Contato> contatos = new ArrayList<Contato>();
-		
-		PreparedStatement stmt = this.conn.prepareStatement("Select * from contatos");
-		ResultSet rs = stmt.executeQuery();
-		while (rs.next()){
-			//criando o objeto Contato
-			Contato contato = new Contato();
-			contato.setId(rs.getLong("id"));
-			contato.setNome(rs.getString("nome"));
-			contato.setEmail(rs.getString("email"));
-			contato.setEndereco(rs.getString("endereco"));
-			Calendar data = Calendar.getInstance();
-			data.setTime(rs.getDate("dataNascimento"));
-			contato.setDataNascimento(data);
-			contatos.add(contato);
+
+	public void remove(Contato contato) {
+		try {
+			PreparedStatement stmt = conn.prepareStatement("delete "+ "from contatos where id=?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-		rs.close();
-		stmt.close();
-		return contatos;
-	}catch (SQLException e){
-		throw new RuntimeException(e);
 	}
-	}
-
 }
---
+++ fj21-jdbc/src/br/com/caelum/jdbc/dao/DAOException.java
@@ -0,0 +1,16 @@
+package br.com.caelum.jdbc.dao;
+
+import java.sql.SQLException;
+
+public class DAOException extends RuntimeException {
+
+	public DAOException(SQLException e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
