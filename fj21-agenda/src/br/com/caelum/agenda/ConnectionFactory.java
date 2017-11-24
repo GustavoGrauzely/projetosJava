bfa69cf2dd7982fa4d63c8451435321af7ff8898
Gustavo Grauzely <ggrauzely.git@gmail.com>
2017-11-24 18:51:44 -0200

    Criação de um webservice rest

---
+++ fj21-agenda/src/br/com/caelum/agenda/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.caelum.agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionFactory {
+	
+	public Connection getConnection() {
+		try{
+			return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "Alv696569");
+    	}catch(SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+
+}
